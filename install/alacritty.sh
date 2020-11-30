8ffd38e0032dc89c8e5669936c682ded471e649f
Sergey Kluchkovsky <kaineer@gmail.com>
2020-11-30 21:22:58 +0500

    Adds alacritty for debian machines

--- install/alacritty.sh
+++ install/alacritty.sh
@@ -1,5 +1,12 @@
 # https://github.com/alacritty/alacritty
 
-sudo add-apt-repository ppa:mmstick76/alacritty
-sudo apt-get update
-sudo apt install alacritty
+if [[ "$(uname -a | grep Ubuntu | wc -l)" == "1" ]]; then
+  sudo add-apt-repository ppa:mmstick76/alacritty
+  sudo apt-get update
+  sudo apt install alacritty
+else
+  wget "https://github.com/alacritty/alacritty/releases/download/v0.3.3/Alacritty-v0.3.3-ubuntu_18_04_amd64.deb" -O "/tmp/Alacritty-v0.3.3-ubuntu_18_04_amd64.deb"
+
+  dpkg -i /tmp/Alacritty-v0.3.3-ubuntu_18_04_amd64.deb
+  rm /tmp/Alacritty-v0.3.3-ubuntu_18_04_amd64.deb
+fi
